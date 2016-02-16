9798fb64cb557e30ffd4e3b151f11506879cfe86
Ext3rmin4tor <fdigiacom@gmail.com>
2016-02-16 17:57:54 +0100

    Added exercises on simple functions and recursion

---
+++ Functional programming exercises/RecursiveFunctionsFSharp/RecursiveFunctionsFSharp/Program.fs
@@ -0,0 +1,172 @@
+﻿//Note: To convert a string to list of characters and from list of characters to string use the following functions:
+//let toCharList (s: string) : char list = s.ToCharArray() |> List.ofArray
+//let toString (cl : char list) : string = List.fold (fun s c -> s + (string c)) "" cl
+
+//Velociraptors were very intelligent animals and, according to extremely reliable scientific sources, they were able to communicate verbally and
+//to perform simple human-like tasks, such as opening doors. At this purpose, since as computer scientists we fear uncertainty, we want to predict how much time we would
+//survive if we are barricaded in a room of a building with a velociraptor roaming around. Opening a door initially takes T seconds for a velociraptor. Every time it opens a new door
+//the required time is halved. Write a function that estimates your survival time if your are stuck in a room (you cannot change room) and the velociraptor is D doors
+//far from you. The function takes as input the initial time T required to open a door and the number of doors to open and returns the seconds you have left to leave. Remember: velociraptors 
+//do not wear out and they do not know fear.
+
+//In the 40th millenium the fleets of the Emperor of mankind will travel across the galaxy to cleanse the Imperium from aliens and heretics threatening the humans. Reaching a point in the galaxy
+//requires a ship to travel at warp speed. Unfortunately, if the endpoint of the course is affected by a warp storm, the ship will be damaged. Although expendable, the Imperium prefers not to throw 
+//away the lives of guardsmen so each captain must know if the course he plotted is valid. At this purpose we model the galaxy as a list of sectors. Each sector is defined by a triple containg the X 
+//and Y coordinates (integer) and the amount of damage the ship takes when jumping there (example: (0,5,30) means that if a ship jumps to sector with X = 0 and Y = 5 it takes 30 damage). 
+//You can think about this as a matrix orginized by rows. Each sector contains the amount of damage the ship takes when jumping there. 
+//If the value is positive than it means there is a warp storm. A ship is destroyed when the damage reaches 100 or more.
+//A course takes as input a list of pairs containing the coordinates of each course point. A course is valid if the ship can cross all the sectors in the course without being destroyed. Write a function
+//takes as input a galaxy, a ship course, and checks if the course is valid.
+
+//Caesar's chypher is the first cryptographic algorithm documented in history. Given a text and an integer n called key, you shift ahead every letter of the text by n positions. If you reach the end of 
+//the alphabet you go back from the beginning. For example the letter 'c' encoded with key = 3 becomes 'f', the letter 'y' encoded with key = 5 becomes the letter 'd'. Write the functions to encode
+//and decode a given text with this algorithm. Decoding a text encoded with Caesar's chypher only requires to apply the encoding by shifting BACK of the same amount the letters.
+//For example if you encoded the text with key = 5 then decoding just requires to call the same function with key = -5 and the encoded text as input. 
+//For simplicity only encode lower case letters, but if you want you can also make a version which encodes capital letters.
+//Punctuations and special characters are never encoded.
+
+
+//Given a string as input write a function that splits it into substrings by splitting it at every whitespace. For example
+//splitSpaces "Hello World!" would return ["Hello"; "World!"]. 
+
+//Generalize the previous function to accept as input a specific character separator for example
+//splitBy "Hello World!" 'o' would return ["Hell"; " w"; "rld!"]
+
+//David is trying to improve his writing skills but he often does the mistake of using the word "besides"
+//instead of "furthermore". Since this problem is impacting his career, he is asking you to make a text checker
+//that replaces every occurence of the word "Besides" with "However". Implement a function which takes as input
+//a text in the format of a string and returns a text where every word besides is replaced by however. Remember that
+//words are always separated by whitespaces. Assume that the letters are never capitalized and there are no punctuation characters. 
+//Generalize the previous function to accept as input the word to be replaced and the word to replace.
+
+let toCharList (s: string) : char list = s.ToCharArray() |> List.ofArray
+let toString (cl : char list) : string = List.fold (fun s c -> s + (string c)) "" cl
+
+let rec velociraptor (openingTime : float) (doors : int) : float =
+  match doors with
+  | 0 -> 0.0
+  | _ -> openingTime + (velociraptor (openingTime / 2.0) (doors - 1))
+
+let validCourse (galaxy : (int * int * int) list) (course : (int * int) list) : bool =
+  let rec findSectorDamage galaxy sector =
+    let sx,sy = sector
+    match galaxy with
+    | [] -> failwith "Not existing sector"
+    | (gx,gy,damage) :: sectors ->
+        if gx = sx && gy = sy then damage else findSectorDamage sectors sector
+  let rec checkCourse galaxy course takenDamage =
+    match course with
+    | [] -> true
+    | sector :: sectors ->
+        let damage = (findSectorDamage galaxy sector) + takenDamage
+        if damage >= 100 then false else checkCourse galaxy sectors damage
+  checkCourse galaxy course 0
+     
+
+
+let splitSpaces text =
+  let rec splitCharList text split acc =
+    match text with
+    | [] -> split @ [acc |> List.rev |> toString]
+    | c :: cs when c = ' ' ->
+        let acc = acc |> List.rev
+        (splitCharList cs (split @ [acc |> toString]) []) 
+    | c :: cs ->
+        splitCharList cs split (c :: acc)
+  splitCharList (toCharList text) [] []
+
+let splitBy text separator =
+  let rec splitCharList text split acc separator =
+    match text with
+    | [] -> split @ [acc |> List.rev |> toString]
+    | c :: cs when c = separator ->
+        let acc = acc |> List.rev
+        splitCharList cs (split @ [acc |> toString]) [] separator
+    | c :: cs ->
+        splitCharList cs split (c :: acc) separator
+  splitCharList (toCharList text) [] [] separator
+
+let textCorrection text =
+  let splitText = (splitBy text ' ')
+  let rec replace text =
+    match text with
+    | [] -> []
+    | word :: words ->
+        if word = "besides" then 
+          "furthermore" :: (replace words) 
+        else 
+          word :: (replace words)
+  let rec combineText text =
+    match text with
+    | [] -> ""
+    | word :: words -> 
+        word + " " + (combineText words)
+  replace splitText |> combineText
+
+let textCorrectionGeneric text word replacer =
+  let splitText = (splitBy text ' ')
+  let rec replace text word replacer =
+    match text with
+    | [] -> []
+    | w :: words ->
+        if w = word then 
+          replacer :: (replace words word replacer) 
+        else 
+          w :: (replace words word replacer)
+  let rec combineText text =
+    match text with
+    | [] -> ""
+    | word :: words -> 
+        word + " " + (combineText words)
+  replace splitText word replacer |> combineText
+
+let (>>) (c : char) (n : int) : char = ((((((int c) - 97 + n) % 26) + 26) % 26) + 97) |> char
+
+let rec encodeCaesar text n =
+  match text with
+  | [] -> []
+  | c :: cs ->
+      if (int c >= 97 && int c <= 122) then
+        (c >> n) :: (encodeCaesar cs n)
+      else
+        c :: (encodeCaesar cs n)
+
+let rec encodeVigenere text password encode =
+  match text, password with
+  | [], _ -> []
+  | c :: cs, p :: ps ->
+      if (int c >= 97 && int c <= 122) then
+        let n = if encode then int p else -(int p)
+        (c >> n) :: (encodeVigenere cs (ps @ [p]) encode)
+      else
+        c :: (encodeVigenere cs (ps @ [p]) encode)
+  | _ -> failwith "uh?!"
+
+
+[<EntryPoint>]
+let main argv =
+  let milkyWay =
+    [
+      (0,0,0)
+      (0,1,10)
+      (0,2,0)
+      (1,0,50)
+      (1,1,0)
+      (1,2,0)
+      (2,0,10)
+      (2,1,0)
+      (2,2,70)
+    ]
+  let course =
+    [
+      (0,0)
+      (0,1)
+      (2,0)
+      (1,0)
+    ]
+  let text = "Arma virumque cano, Troiae qui primus ab oris Italiam, fato profugus, Laviniaque venit litora." |> toCharList
+  let password = "e" |> toCharList
+  let encodedText = encodeVigenere text password true |> toString
+  let decodedText = encodeVigenere (encodedText |> toCharList) password false |> toString
+  printfn "%A \n%A" encodedText decodedText
+  0 // return an integer exit code
---
+++ Functional programming exercises/SimpleFunctionsFSharp/SimpleFunctionsFSharp/Program.fs
@@ -0,0 +1,103 @@
+﻿//1. Define a function that takes two integers n, m and another integer x and do the following: if the product of n and x is m return the string
+//"Tro", if the product of m and x is n return "lolo", and if the product of n and m is x return "Trololo". In all other cases just return an emtpy string.
+
+//2. Define a new version of exercise 1 where the user can specify the messages to output.
+
+//3. The crazy operation takes two integer numbers n and m and does the following: if n divides m then just return an integer number containing the digits nm.
+//For example crazy 4 8 returns 48. If n is positive and m negative return their product. In all the other cases return the sum of the two numbers. Implement this
+//extremely useful operation.
+
+//4. A year is a "leap year" if it is divisible by 4, with the exception of century years (those divisible by 100) that are not divisible by 400. Write a function that
+//takes as input a year and returns true if it is a leap year.
+
+//5. The prisoner's dilemma is a historical game analyzed in game theory: two prisoners are asked to confess their crimes. Depending on their answers they must serve in
+// prison in the following ways:
+// a) If both betray each other they both serve 2 years.
+// b) If one of the two confesses and the other stays silent, the one who confessed will be set free and the other will serve 3 years.
+// c) If both stay stays silent, both of them will serve 1 year in prison.
+// Write a function that takes as input whether each prisoner confessed or not, and output the years to serve for one of the prisoners depending on the answer of the other one.
+
+//6. An obsessive compulsive person is a mentally sick person who does some routine rituals when some specific situation presents itself. Bob has an obsessive compulsive desease
+// which makes him react in the following ways:
+// a) When he is going to the toilet he does the following: if the toilet seat is up then he puts it down and reverse the toilet paper roll. Otherwise he lifts up the toilet seat
+// but he does NOT reverse the toilet paper roll.
+// b) When he is walking on the street, if the pavement has tiles then he jumps to avoid tile intersections, if the pavement is grey but without tiles he walks backward, otherwise he
+// walks normally.
+// c) When he is going to the restaurant, if table 42 is empty he seats there, if it is busy he insults the waiter and leaves.
+// Write a function that takes as input a string depicting a situation (for example "at toilet"), and a state (for example "toilet seat up") and outputs Bob's reaction as a string
+// i.e. "insult waiter". If you input an invalid combination of situation-event then Bob becomes crazy and kills himself.
+
+
+let exercise1 n m x =
+  if n * x = m then
+    "Tro"
+  elif m * x = n then
+    "lolo"
+  elif n * m = x then
+    "Trololo"
+  else
+    ""
+
+let exercise2 n m x tro lolo trololo _default : string =
+  if n * x = m then
+    tro
+  elif m * x = n then
+    lolo
+  elif n * m = x then
+    trololo
+  else
+    _default
+
+let exercise3 n m =
+  if m % n = 0 then
+    n * 10 + m
+  elif n > 0 && m < 0 then
+    n * m
+  else
+    n + m
+
+let exercise4 year =
+  if year % 4 = 0 then
+    if year % 100 = 0 then
+        not (year % 400 <> 0) 
+    else
+      true
+  else
+    false
+
+let exercise5 answer1 answer2 =
+  match answer1, answer2 with
+  | true, true -> 2
+  | true, false -> 0
+  | false, true -> 3
+  | false, false -> 1
+
+let exercise6 situation event =
+  match situation with
+  | "at toilet" ->
+      match event with
+      | "toilet seat up" -> "put down toilet seat and reverse roll"
+      | "toilet seat down" -> "lift up seat"
+      | _ -> "get crazy and kill himself"
+  | "walking" ->
+      match event with
+      | "pavement has tiles" -> "jump from tile to tile"
+      | "grey pavement" -> "walk backward"
+      | _ -> "walk normally"
+  | "at restaurant" ->
+      match event with
+      | "table 42 empty" -> "seat down"
+      | "table 42 busy"-> "insult the waiter and leave"
+      | _ -> "get crazy and kill himself"
+  | _ -> "get crazy and kill himself"    
+
+
+
+
+
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" (exercise6 "at toilet" "toilet seat down")
+    0 // return an integer exit code
