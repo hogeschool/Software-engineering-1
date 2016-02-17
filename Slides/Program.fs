70c804a596839a07d6471f61d62645a1070126bd
giuseppemag <giuseppemag@gmail.com>
2016-02-17 16:29:09 +0100

    Fixed main.

--- Slides/Program.fs
+++ Slides/Program.fs
@@ -6,7 +6,10 @@ let main argv =
 //  do batchProcess FunctionalWeek2.slides "INFSEN02-1-Lec2-Delta-rules" "The INFDEV@HR Team" "Delta rules" false false
 //  do batchProcess FunctionalWeek3.slides "INFSEN02-1-Lec3-Data-structures" "The INFDEV@HR Team" "Data structures" false false
 //  do batchProcess FunctionalWeek4.slides "INFSEN02-1-Lec4-Let-Letrec-FSharp" "The INFDEV@HR Team" "Let, let-rec, and F\# translations" false false
-  do batchProcess FunctionalWeek4.slides "INFSEN02-1-Lec5-Types" "The INFDEV@HR Team" "Types, inference, and F\# data types" true false
+  do batchProcess FunctionalWeek5.slides "INFSEN02-1-Lec5-Types" "The INFDEV@HR Team" "Types, inference, and F\# data types" true false
+  // add notice about homework
+  // some terms in F# rendering are too verbose, and should be on a single line
+
   // Practicum 1
   // Lecture 7 Haskell translations and laziness
   // Practicum 2
